7bdf4cd6bd48a17f4ec9359302b8d23df4649b45
Nicholas Hemsley <nick.hems@gmail.com>
2025-04-28 21:51:18 +1000

    Simplify get_opencl_c_code() to always read from kernel_formatted.cl

--- src/kernel.hpp
+++ src/kernel.hpp
@@ -5,16 +5,7 @@
 
 string opencl_c_container(); // outsourced to kernel.cpp
 string get_opencl_c_code() {
-	string r = opencl_c_container();
-	r = replace(r, " ", "\n"); // replace all spaces by new lines
-	r = replace(r, "#ifdef\n", "#ifdef "); // except for the arguments after some preprocessor options that need to be in the same line
-	r = replace(r, "#ifndef\n", "#ifndef ");
-	r = replace(r, "#define\n", "#define "); // #define with two arguments will not work
-	r = replace(r, "#undef\n", "#undef ");
-	r = replace(r, "#if\n", "#if "); // don't leave any spaces in arguments
-	r = replace(r, "#elif\n", "#elif "); // don't leave any spaces in arguments
-	r = replace(r, "#pragma\n", "#pragma ");
-	return "\n"+r;
+	return read_file("src/kernel_formatted.cl");
 }
 
 // everything below is just for syntax highlighting in the editor, this does not change any functionality
